70f032e38b941aaa2dececc9abc480ff8f642869
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-17 08:01:40 -0500

    commit tree cd524f49d026c45a5b0258f76e07e53ac28973e0

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if ((feet > 400) && (feet < 2000)) {
+    return 'That will be twenty bucks.';
+  } else if ((feet > 2000) && (feet < 2500)) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
+};
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return ((city === 'NYC') ? 'Ok, sounds good.' : 'No go.');
+ 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case ('generous') :
+      return 'Thank you so much.'
+      break;
+    case ('not as generous') :
+      return 'Thank you.'
+      break;
+    default: 
+      return 'Bye.'
+      
+  }
 }
